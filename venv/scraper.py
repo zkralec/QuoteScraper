d941305583edc68fd26753fb810ec36feb98e537
zkralec <zkralec@icloud.com>
2025-06-13 14:24:25 -0400

    Finished version of the QuoteScraper program

--- venv/scraper.py
+++ venv/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import csv
+
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 # GET request
 url = 'https://quotes.toscrape.com'
@@ -17,18 +19,37 @@ quote_file = open('quotes.csv', mode='w', newline='', encoding='utf-8')
 writer = csv.writer(quote_file)
 writer.writerow(['Quote', 'Author'])
 
-# Creating empty list
-list = []
+# Getting next page block
+next_block = soup.find_all('li', 'next')
 
-# Looping through the block and extracting the quote and author block
-for i in quote_block:
-    quote = i.find('span', 'text').get_text()
-    author = i.find('small', 'author').get_text()
-    list.append([quote, author])
+# Finding the correct tag and getting the joinable href
+for i in next_block:
+    temp = i.find('a')
+    href = temp.get('href')
 
-# Looping through list and writing each quote/author
-for i in list:
-    writer.writerow(i)
+while href is not None:
+    
+    # Request new url
+    response = requests.get(urljoin(url, href))
+
+    # Parsing ne HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get the block with the quote
+    quote_block = soup.find_all('div', 'quote')
+
+    # Looping through the block and extracting the quote and author block
+    for i in quote_block:
+        quote = i.find('span', 'text').get_text()
+        author = i.find('small', 'author').get_text()
+        writer.writerow([quote, author])
+
+    # Getting next block and hyperlink reference
+    next_block = soup.find('li', 'next')
+
+    # Set the hyperlink reference if there is a next button, else stop
+    href = next_block.find('a')['href'] if next_block else None
 
 # Closing the CSV file
 quote_file.close()
+print("DONE: Check the 'quotes.csv' file for all scraped quotes")
